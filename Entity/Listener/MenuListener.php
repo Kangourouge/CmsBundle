2c4700b76f1216e67e6a6ca3264685ddf51612cb
aminiconi <aminiconi@kangourouge.com>
2018-05-04 15:14:54 +0200

    [FIX] Menu cache listener

---
+++ Entity/Listener/MenuListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace KRG\CmsBundle\Entity\Listener;
+
+use Doctrine\Common\EventArgs;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Events;
+use KRG\CmsBundle\Entity\MenuInterface;
+use KRG\CmsBundle\Menu\MenuBuilder;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+class MenuListener implements EventSubscriber
+{
+    /** @var FilesystemAdapter */
+    protected $cache;
+
+    public function __construct($twigCacheDir)
+    {
+        $this->cache = new FilesystemAdapter(MenuBuilder::CACHE_NAMESPACE, 0, $twigCacheDir);
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::postUpdate,
+        ];
+    }
+
+    public function postUpdate(LifecycleEventArgs $event)
+    {
+        if ($event->getEntity() instanceof MenuInterface) {
+            $this->cache->deleteItem($event->getEntity()->getRootParent()->getKey());
+        }
+    }
+}
