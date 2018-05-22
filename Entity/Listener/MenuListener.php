37f2337c83cf0e0bdf04d895f95390b621761b03
aminiconi <aminiconi@kangourouge.com>
2018-05-22 17:36:12 +0200

    [FEAT] Menu key generator

--- Entity/Listener/MenuListener.php
+++ Entity/Listener/MenuListener.php
@@ -3,11 +3,11 @@
 namespace KRG\CmsBundle\Entity\Listener;
 
 use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 use KRG\CmsBundle\Entity\MenuInterface;
-use KRG\CmsBundle\Menu\MenuBuilder;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class MenuListener implements EventSubscriber
@@ -15,19 +15,62 @@ class MenuListener implements EventSubscriber
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
 
-    /**
-     * MenuListener constructor.
-     *
-     * @param EventDispatcherInterface $eventDispatcher
-     */
-    public function __construct(EventDispatcherInterface $eventDispatcher)
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, EntityManagerInterface $entityManager)
     {
         $this->eventDispatcher = $eventDispatcher;
+        $this->entityManager = $entityManager;
     }
 
     public function getSubscribedEvents()
     {
-        return [Events::postUpdate];
+        return [
+            Events::preUpdate,
+            Events::prePersist,
+            Events::postUpdate
+        ];
+    }
+
+    public function prePersist(LifecycleEventArgs $event)
+    {
+        if ($event->getEntity() instanceof MenuInterface) {
+            /** @var $menu MenuInterface */
+            $menu = $event->getEntity();
+            if (strlen($menu->getKey()) === 0) {
+                $menu->setKey($this->generateKey($menu));
+            }
+        }
+    }
+
+    public function preUpdate(PreUpdateEventArgs $event)
+    {
+        if ($event->getEntity() instanceof MenuInterface) {
+            /** @var $menu MenuInterface */
+            $menu = $event->getEntity();
+            if (strlen($menu->getKey()) === 0) {
+                $menu->setKey($this->generateKey($menu));
+            }
+        }
+    }
+
+    protected function generateKey(MenuInterface $menu, $index = 0)
+    {
+        $prefix = '';
+        /** @var $parent MenuInterface */
+        if ($parent = $menu->getParent()) {
+            $prefix = $parent->getKey().'_';
+        }
+
+        $suffix = $index > 0 ? '_'.$index : '';
+
+        $key = sprintf('%s%s%s', $prefix, self::underscoreCase($menu->getName()), $suffix);
+        if ($this->entityManager->getRepository(MenuInterface::class)->findOneBy(['key' => $key]) === null) {
+            return $key;
+        }
+
+        return self::generateKey($menu, $index + 1);
     }
 
     public function postUpdate(LifecycleEventArgs $event)
@@ -35,5 +78,15 @@ class MenuListener implements EventSubscriber
         if ($event->getEntity() instanceof MenuInterface) {
             $this->eventDispatcher->dispatch('cache:clear:data');
         }
+    }
+
+    public static function underscoreCase($string)
+    {
+        $string = preg_replace('/[^a-z0-9]+/i', ' ', $string);
+        $string = trim($string);
+        $string = str_replace(' ', '_', $string);
+        $string = strtolower($string);
+
+        return $string;
     }
 }
