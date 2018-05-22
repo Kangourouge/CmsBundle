76061d8646c06457707c81c0c7dfd26bcc5291d0
aminiconi <aminiconi@kangourouge.com>
2018-05-22 18:00:02 +0200

    [FIX] Menu key generator

--- Entity/Listener/MenuListener.php
+++ Entity/Listener/MenuListener.php
@@ -82,6 +82,7 @@ class MenuListener implements EventSubscriber
 
     public static function underscoreCase($string)
     {
+        $string = strtr(utf8_decode($string), utf8_decode('àáâãäçèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝ'), 'aaaaaceeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUY');
         $string = preg_replace('/[^a-z0-9]+/i', ' ', $string);
         $string = trim($string);
         $string = str_replace(' ', '_', $string);
