145fe64e3af597c31b73e5a6f2dfc0da13222cd6
aminiconi <aminiconi@kangourouge.com>
2018-05-24 17:23:34 +0200

    [FEAT] Block key auto generate

--- Entity/Listener/BlockListener.php
+++ Entity/Listener/BlockListener.php
@@ -2,10 +2,12 @@
 
 namespace KRG\CmsBundle\Entity\Listener;
 
-use Doctrine\Common\EventSubscriber;
-use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
-use Doctrine\ORM\Events;
+use Doctrine\ORM\EntityManagerInterface;
+use KRG\CmsBundle\Util\Str;
 use KRG\CmsBundle\Entity\BlockInterface;
+use Doctrine\ORM\Events;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class BlockListener implements EventSubscriber
@@ -13,19 +15,19 @@ class BlockListener implements EventSubscriber
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
 
-    /**
-     * BlockListener constructor.
-     *
-     * @param EventDispatcherInterface $eventDispatcher
-     */
-    public function __construct(EventDispatcherInterface $eventDispatcher)
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, EntityManagerInterface $entityManager)
     {
         $this->eventDispatcher = $eventDispatcher;
+        $this->entityManager = $entityManager;
     }
 
     public function getSubscribedEvents()
     {
         return [
+            Events::prePersist,
             Events::postPersist,
             Events::postUpdate,
             Events::preUpdate,
@@ -35,29 +37,44 @@ class BlockListener implements EventSubscriber
 
     public function postPersist(LifecycleEventArgs $event)
     {
-        if ($event->getObject() instanceof BlockInterface) {
+        if ($event->getEntity() instanceof BlockInterface) {
             $this->clearCache();
+        }
+    }
+
+    public function prePersist(LifecycleEventArgs $event)
+    {
+        if ($event->getEntity() instanceof BlockInterface) {
+            $this->prePersistOrUpdate($event->getEntity());
         }
     }
 
     public function preUpdate(LifecycleEventArgs $event)
     {
-        if ($event->getObject() instanceof BlockInterface) {
-            $event->getObject()->setWorking(true);
+        if ($event->getEntity() instanceof BlockInterface) {
+            $event->getEntity()->setWorking(true);
+            $this->prePersistOrUpdate($event->getEntity());
+        }
+    }
+
+    public function prePersistOrUpdate(BlockInterface $block)
+    {
+        /** @var $menu BlockInterface */
+        if (strlen($block->getKey()) === 0) {
+            $block->setKey($this->generateKey($block));
         }
     }
 
     public function postUpdate(LifecycleEventArgs $event)
     {
-        if ($event->getObject() instanceof BlockInterface) {
+        if ($event->getEntity() instanceof BlockInterface) {
             $this->clearCache();
-
         }
     }
 
     public function postRemove(LifecycleEventArgs $event)
     {
-        if ($event->getObject() instanceof BlockInterface) {
+        if ($event->getEntity() instanceof BlockInterface) {
             $this->clearCache();
         }
     }
@@ -65,5 +82,18 @@ class BlockListener implements EventSubscriber
     protected function clearCache()
     {
         $this->eventDispatcher->dispatch('cache:clear:twig');
+    }
+
+    protected function generateKey(BlockInterface $menu, $index = 0)
+    {
+        $suffix = $index > 0 ? '_'.$index : '';
+        $key = sprintf('%s%s', Str::underscoreCase($menu->getName()), $suffix);
+        $key = (strlen($key) > 200) ? substr($key, 0, 200) : $key;
+
+        if ($this->entityManager->getRepository(BlockInterface::class)->findOneBy(['key' => $key]) === null) {
+            return $key;
+        }
+
+        return $this->generateKey($menu, $index + 1);
     }
 }
