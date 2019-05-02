dcb8474d259bfc4973e8e516fcca1403694b3f31
aminiconi <aminiconi@kangourouge.com>
2019-05-02 17:49:07 +0200

    [FIX] PostFlush clear routing only

--- Entity/Listener/SeoListener.php
+++ Entity/Listener/SeoListener.php
@@ -41,6 +41,6 @@ class SeoListener implements EventSubscriber
 
     public function postFlush(PostFlushEventArgs $event)
     {
-        $this->eventDispatcher->dispatch('cache:clear');
+        $this->eventDispatcher->dispatch('cache:clear:routing');
     }
 }
